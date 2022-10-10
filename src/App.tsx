2501149fe596c9aa96afed5b44b7750c2b23f06b
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-10-10 11:07:12 +0200

    Add monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+interface Ec2Data {
+  pid: string
+  cpu: number
+  user: string
+}
 
 function App() {
+  const [ec2Data, setEc2Data] = useState<Ec2Data[]>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    fetch("/monitoring-data.json")
+      .then(response => response.json())
+      .then(setEc2Data)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main >
+      <h1>Fancy Dashboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ec2 instance</th>
+            <th>cpu</th>
+            <th>memory</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ec2Data?.map(row => 
+          <tr key={row?.pid || '-'}>
+            <td>
+              {row?.pid || '-'}
+            </td>
+            <td>
+              {row?.cpu || '-'}%
+            </td>
+
+            <td>
+              {row?.user || '-'}
+            </td>
+          </tr>
+          )}
+        </tbody>
+      </table>
+      {error && <p>failed to load data</p>}
+      {loading &&
+        //@ts-ignore
+        <progress intermidate="true"></progress>}
+    </main>
   );
 }
 
